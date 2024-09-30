c3cb03a09ad4c327839ac331985701c40abd3435
Gaurav-Krishna-Gaali <your_email_address@example.comcls>
2024-09-30 17:04:55 +0530

    Added realtime database neon

--- app/(api)/(stripe)/pay+api.ts
+++ app/(api)/(stripe)/pay+api.ts
@@ -1,30 +1,26 @@
-import { Alert } from "react-native";
 import { Stripe } from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
-export async function POST(req: Request) {
+export async function POST(request: Request) {
   try {
-    const body = await req.json();
-    const { payment_method_id, paymnent_intent_id, customer_id } = body;
+    const body = await request.json();
+    const { payment_method_id, payment_intent_id, customer_id, client_secret } =
+      body;
 
-    if (!payment_method_id || !paymnent_intent_id || !customer_id) {
+    if (!payment_method_id || !payment_intent_id || !customer_id) {
       return new Response(
-        JSON.stringify({
-          error: "Missing required payment information ",
-          status: 400,
-        })
+        JSON.stringify({ error: "Missing required fields" }),
+        { status: 400 }
       );
     }
 
     const paymentMethod = await stripe.paymentMethods.attach(
       payment_method_id,
-      {
-        customer: customer_id,
-      }
+      { customer: customer_id }
     );
 
-    const result = await stripe.paymentIntents.confirm(paymnent_intent_id, {
+    const result = await stripe.paymentIntents.confirm(payment_intent_id, {
       payment_method: paymentMethod.id,
     });
 
@@ -36,13 +32,9 @@ export async function POST(req: Request) {
       })
     );
   } catch (error) {
-    console.log("error", { error });
-
-    return new Response(
-      JSON.stringify({
-        error: error,
-        status: 400,
-      })
-    );
+    console.error("Error paying:", error);
+    return new Response(JSON.stringify({ error: "Internal Server Error" }), {
+      status: 500,
+    });
   }
 }
